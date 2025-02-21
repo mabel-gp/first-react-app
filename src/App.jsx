e2e3eb2ce5be024653fea7cbf3d57415ecff9fe5
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2025-02-20 23:20:02 -0500

    First component and add styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import './App.css';
 import Testimony from './components/testimony.jsx';
 
 function App() {  // Este es un componente
-
-  //const [count, setCount] = useState(0)
-
   return (
     <div className='App'>
-      <Testimony/>
+      <div className='principal-container'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimony/>
+      </div>
     </div>
   )
 }
--- src/components/testimony.jsx
+++ src/components/testimony.jsx
@@ -1,5 +1,5 @@
 import emmaImage from '../images/testimony-emma.png';
-
+import '../style-sheets/testimony.css';
 
 // Componente Funcional (es una fx de JS)
 
@@ -8,11 +8,11 @@ function Testimony(){
   return(
     // Contenedor principal
     <div className='container-testimony'>
-      <img className='images-testimony' 
+      <img className='image-testimony' 
       src={emmaImage}
       alt='Imagen de Emma'/>
       <div className='container-text-testimony'>
-        <p className='nombre-testominy'>Emma Bostian en Suecia</p>
+        <p className='name-testimony'>Emma Bostian en Suecia</p>
         <p className='job-testimony'>Ingeniera de Software en Spotify</p>
         <p className='text-testimony'>
           Siempre me costó aprender JavaScript. Hice muchos cursos, pero el de freeCodeCamp fue el que me quedó grabado. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp, me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</p>
